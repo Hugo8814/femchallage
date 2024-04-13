3020ca01ffe8bd6b6845773863439fa11e6f2c61
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-04-12 21:52:58 -0300

    idk

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
   return (
     <>
-    <div className='container'>
-
-      <Title/>
-      <Search/>
-    <h3>
-      Search for any IP address or domain
-      IP Address
-      Location
-      Timezone
-      UTC 
-      ISP
-    </h3>
-    </div>
+      <div className='container'>
+        <Title/>
+        <Search/>
+        <h3>
+          Search for any IP address or domain
+          IP Address
+          Location
+          Timezone
+          UTC 
+          ISP
+        </h3>
+      </div>
     </>
   );
 }
+
 function Title() {
   return (
     <>
-    <h1 className='title'>IP Address Tracker</h1>
-    </>
-  );
-}
-function Search() {
-  return (
-    <>
-    <div className='searchbar-box'>
-    <input type="text" placeholder="Search for any IP address or domain"  className='searchbar-input'/>
-    <div className='search'>
-    <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
-    </div>
-    </div>
+      <h1 className='title'>IP Address Tracker</h1>
     </>
   );
 }
 
+function Search() {
+  return (
+    <>
+      <div className='searchbar-box'>
+        <input type="text" placeholder="Search for any IP address or domain"  className='searchbar-input'/>
+        <div className='search'>
+          <FontAwesomeIcon icon={faChevronRight} /> {/* Use the specific icon */}
+        </div>
+      </div>
+    </>
+  );
+}
 
 export default App;
